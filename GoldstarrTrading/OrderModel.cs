f1e2e2d45862d35b5dd49474e544ee11dcd45022
Andreas Linné <andreas_linne@hotmail.com>
2020-10-29 23:29:11 +0100

    Added OrderModel

---
+++ GoldstarrTrading/OrderModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldstarrTrading
+{
+    class OrderModel
+    {
+        private DateTime OrderDate { get; set; }
+        private string CustomerName { get; set; }
+        private Merchandise Merch { get; set; }
+        private int OrderedAmount { get; set; }
+
+        private void AddMerchandiseToOrder(Merchandise merchandise)
+        {
+
+        }
+
+    }
+}
