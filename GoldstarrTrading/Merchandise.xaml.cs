00d49fbd677bbc152e2f0adc93f84145d74854f4
LAPTOP-99FUU4G4\aleks <alekss.radinovic@hotmail.com>
2020-11-04 10:14:56 +0100

    Lade till knappar som lägger till nya produkter i merchandiseList

--- GoldstarrTrading/Merchandise.xaml.cs
+++ GoldstarrTrading/Merchandise.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using GoldstarrTrading.Classes;
+using System.Collections.ObjectModel;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,7 +25,8 @@ namespace GoldstarrTrading
     /// </summary>
     public sealed partial class Merchandise : Page
     {
-        
+        private ViewModel vm { get; set; }
+
         /// <summary>
         /// Binding Merchandise list to merchandiseList listview
         /// </summary>
@@ -31,9 +34,57 @@ namespace GoldstarrTrading
         {
             this.InitializeComponent();
         }
+
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            vm = ((ViewModel)e.Parameter);
             merchandiseList.ItemsSource = ((ViewModel)e.Parameter).ObsMerch;
         }
+
+        private void AddProductsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var message = string.Empty;
+            MessageDialog messageDialog = new MessageDialog(message,"New product has been added");
+            var supplier = string.Empty;
+            var product = string.Empty;
+
+            if(string.IsNullOrEmpty(SupplierNameTextBox.Text) ||
+                string.IsNullOrEmpty(ProductNameTextBox.Text))
+            {
+                message = "Data is missing";
+                messageDialog = new MessageDialog(message, "Information");
+                messageDialog.ShowAsync();
+                return;
+            }
+
+            if(SupplierNameTextBox.Text.All(char.IsLetter))
+                supplier = SupplierNameTextBox.Text;
+            
+            else
+            {
+                message = "Invalid supplier data";
+                messageDialog = new MessageDialog(message, "Information");
+                messageDialog.ShowAsync();
+                SupplierNameTextBox.Text = string.Empty;
+                return;
+            }
+            if (ProductNameTextBox.Text.All(char.IsLetter))
+                product = ProductNameTextBox.Text;
+            else
+            {
+                message = "Invalid product data";
+                messageDialog = new MessageDialog(message, "Information");
+                messageDialog.ShowAsync();
+                ProductNameTextBox.Text = string.Empty;
+                return;
+            }
+            
+            vm.ObsMerch.Add(new MerchandiseModel() { Supplier = supplier, ProductName = product });
+            messageDialog.ShowAsync();
+            SupplierNameTextBox.Text = string.Empty;
+            ProductNameTextBox.Text = string.Empty;
+        }
+
     }
 }
