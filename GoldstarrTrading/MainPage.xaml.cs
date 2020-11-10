8e1e7182792020a7ab0cc0bc5b3eada3fe58bf6d
Andreas Linné <andreas_linne@hotmail.com>
2020-11-10 11:56:01 +0100

    Fixed date not showing correctly in order history

--- GoldstarrTrading/MainPage.xaml.cs
+++ GoldstarrTrading/MainPage.xaml.cs
@@ -62,11 +62,11 @@ namespace GoldstarrTrading
         {
             viewModel.CustomerList = new ObservableCollection<CustomerModel>()
             {
-            new CustomerModel { Name = "Damien Satansson", Address = "Helsikesgatan 666", Phone = "0705-666 666" },
-            new CustomerModel { Name = "Svenne Svensson", Address = "Perssons Väg 13", Phone = "0704-111 222" },
-            new CustomerModel { Name = "Lotta Bråkmakarsson", Address = "Bråkmakargatan 8", Phone = "0706-987 456" },
-            new CustomerModel { Name = "Abdi Abdi", Address = "Guddomliga Gatan 42", Phone = "0707-777 777" },
-            new CustomerModel { Name = "Snurre Sprätt", Address = "Morotsgatan 1", Phone = "0702-123 456" }
+            //new CustomerModel { Name = "Damien Satansson", Address = "Helsikesgatan 666", Phone = "0705-666 666" },
+            //new CustomerModel { Name = "Svenne Svensson", Address = "Perssons Väg 13", Phone = "0704-111 222" },
+            //new CustomerModel { Name = "Lotta Bråkmakarsson", Address = "Bråkmakargatan 8", Phone = "0706-987 456" },
+            //new CustomerModel { Name = "Abdi Abdi", Address = "Guddomliga Gatan 42", Phone = "0707-777 777" },
+            //new CustomerModel { Name = "Snurre Sprätt", Address = "Morotsgatan 1", Phone = "0702-123 456" }
             };
         }
 
